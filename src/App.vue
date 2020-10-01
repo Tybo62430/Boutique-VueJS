80198f19aa531650845a0b9ae7f11187689d0165
derbomez thibault <derbomez.thibault@busi-part.com>
2020-10-01 15:01:47 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <the-header></the-header>
+  </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    TheHeader,
+  },
 };
 </script>
 
